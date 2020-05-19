0a04293aae8227cd8bd5ef3e8e4f2992a81282fb
smartlizzard <pradhanraghunath91@gmail.com>
2020-05-20 00:52:58 +0530

    Update vault.sh

--- vault.sh
+++ vault.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --name ARM-SUBSCRIPTION-ID --vault-name insightdemo --query value -o tsv)
-export ARM_CLIENT_ID=$(az keyvault secret show --name ARM-CLIENT-ID --vault-name insightdemo --query value -o tsv)
-export ARM_CLIENT_SECRET=$(az keyvault secret show --name ARM-CLIENT-SECRET --vault-name insightdemo --query value -o tsv)
-export ARM_TENANT_ID=$(az keyvault secret show --name ARM-TENANT-ID --vault-name insightdemo --query value -o tsv)
-export ARM_ACCESS_KEY=$(az keyvault secret show --name STORAGE-ACCOUNT-KEY --vault-name insightdemo --query value -o tsv)
+#export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --name ARM-SUBSCRIPTION-ID --vault-name insightdemo --query value -o tsv)
+#export ARM_CLIENT_ID=$(az keyvault secret show --name ARM-CLIENT-ID --vault-name insightdemo --query value -o tsv)
+#export ARM_CLIENT_SECRET=$(az keyvault secret show --name ARM-CLIENT-SECRET --vault-name insightdemo --query value -o tsv)
+#export ARM_TENANT_ID=$(az keyvault secret show --name ARM-TENANT-ID --vault-name insightdemo --query value -o tsv)
+#export ARM_ACCESS_KEY=$(az keyvault secret show --name STORAGE-ACCOUNT-KEY --vault-name insightdemo --query value -o tsv)
+
+export ARM_CLIENT_ID=
+export ARM_CLIENT_SECRET=
+export ARM_TENANT_ID=
+export ARM_SUBSCRIPTION_ID=
+export ARM_ACCESS_KEY=
