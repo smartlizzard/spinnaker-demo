ca22c1eee3b786b43aed1dddd65fc6f9217263c7
smartlizzard <pradhanraghunath91@gmail.com>
2020-05-19 19:12:59 +0530

    update

--- aks/vars.tf
+++ aks/vars.tf
@@ -1,21 +1,26 @@
 variable "name" {
   default = "GIVE_A_NAME"
+  description= "This will used for create ACR and its service principle"
 }
+
 variable "location" {
   default = "eastus"
 }
 variable "cluster_name" {
-    default = "YOUR_CLUSTER_NAME"  
+  default = "YOUR_CLUSTER_NAME"  
+  description= "Name of the cluster"
 }
 variable "dns_prefix" {
   default = "SOMETHING"
 }
 variable "node_count" {
   default = "1"
+  description= "No of k8s cluster node"
 }
 
 variable "ResourceGroup" {
   default = "YOUR_RESOURCE_GROUP"
+  description= "This Resource group will create"
 }
 
 variable "tag" {
@@ -23,7 +28,18 @@ variable "tag" {
 }
 variable "VAULT_NAME" {
   default = "GIVE_VAULT_NAME"
+  description= "Name of the vault"
 }
 variable "storage_account" {
   default = "STORAGE_ACCOUNT_NAME"
+  description= "Put storage account where backend tfstate will store"
 }
+variable "container_name" {
+  default = "CONTAINER_NAME"
+  description= "Put container name where backend tfstate will store"
+}
+variable "vault_resource_group" {
+  default = "VAULT_RESOURCE_GROUP"
+  description= "Put your vault resource group name"
+}
+
